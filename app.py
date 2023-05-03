65ec74b41d7b5c5f57c30c812fe062b90e70a841
Gustavo Nascimento <113242581+GustavoNascimento98@users.noreply.github.com>
2023-05-03 10:39:48 -0300

    creating answers section of CEO's question

--- app.py
+++ app.py
@@ -35,6 +35,31 @@ def create_dataframe_section(df):
 
     return None
 
+def create_answers_section(df):
+    st.title("Main Questions Answers")
+
+    st.header("First Round")
+    st.subheader("How many bikes are being sold by their owners and how many bikes are being sold by distributors?")
+
+    st.subheader("How many bikes are being sold are bikes from a unique owner?")
+
+    st.subheader("Are high kilometer bikes more expensive than bikes with lower kilometer?")
+
+    st.subheader("Are the bikes with a unique owner more expense on avarege than the other bikes?")
+
+    st.subheader("Are the bikes that have more owners also the bikes with more kilometers traveled on avarege?")
+
+    st.subheader("Which company has the most bikes registered?")
+
+    st.subheader("Which company has the most expensive bikes on avarege?")
+
+    st.subheader("Are the company that has the most expensive bikes registered also the company with the most bikes registered?")
+
+    st.subheader("Which bikes are good for buying?")
+    
+    return None
+
+
 def main():
     df_raw = load_data()
 
